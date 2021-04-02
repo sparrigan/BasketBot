6d9f7496597cb974083a078a2ce376f6bcad97c5
Nic <sparrigan@gmail.com>
2021-04-02 12:16:42 +0100

    Close #6

--- tests/test_model.py
+++ tests/test_model.py
@@ -4,21 +4,7 @@ Test that we can obtain required information from DB model
 
 import validators
 from basketbot import datamodel as dm
-# from basketbot.util import url_check
-#
-# def test_ext(db_session):
-#     item = dm.Item(name="testing")
-#     item.regions = [dm.Region()]
-#     db_session.add(item)
-#     db_session.commit()
-#
-#     results = dm.Item.query.all()
-#     assert len(results)==1
-#
-# def test_ext2(db_session):
-#     results = dm.Item.query.all()
-#     assert len(results)==0
-#
+
 def test_basket_version_update_on_addition(db_with_items):
     """
     Test that adding an item updates the basket versions
@@ -75,9 +61,6 @@ def test_basket_version_update_on_addition(db_with_items):
     db_with_items.delete(item)
     db_with_items.commit()
     check_region_basket_versions([region_2, region_3], all_regions, init_versions)
-
-
-
     
 
 def check_region_basket_versions(updated_regions, all_regions, init_versions):
@@ -106,19 +89,8 @@ def check_region_basket_versions(updated_regions, all_regions, init_versions):
     for region in [r for r in all_regions if r not in updated_regions]:
         assert region.basket_version == init_versions[region.name]
 
-# def test_scraping_url(database):
-#     """
-#     Check for method returning valid item URLs
-#     """
-#     item_urls = dm.ItemURL.query.all()
-#     for item_url in item_urls:
-#         url = item_url.get_item_url()
-#         assert validators.url()
-#
 # def test_region_price(database):
 #     """
 #     Check for method returning basket price for a region 
 #     """
 #     pass
-
-# def test_updating_basket_version()
